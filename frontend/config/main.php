1042ecb75d3a9441587100f56d87c1ba94188b6b
sepehr.k12 <sepehr.k12@gmail.com>
2019-01-01 10:45:10 +0330

    added urlManager setting to frontend/main instead of common/main-local

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+        // 'urlManager' => [
+        //     'enablePrettyUrl' => true,
+        //     'showScriptName' => false,
+        //     'rules' => [
+        //             '<controller:\w+>/<id:\d+>' => '<controller>/view',
+        //             '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+        //             '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+        
+        //     ],
+        // ],
+        
     ],
     'params' => $params,
 ];
